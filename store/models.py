aa9c51855d92c07a135793f7d29c9666b6a303aa
ShyhiemGilbert <42538473+ShyhiemGilbert@users.noreply.github.com>
2021-09-13 18:54:30 +0100

    One to many relationship classes

--- store/models.py
+++ store/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    
 class Product(models.Models):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -50,6 +55,21 @@ class Order(models.Model):
                                       choices = PAYMENT_STATUS_CHOICES,
                                       default = PAYMENT_STATUS_PENDING 
                                     )
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete= models.PROTECT)
+    product = models.ForeignKey(Product, on_delete= models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+class Cart(models.Model):
+    created_by = models.DateTimeField(auto_now_add=True)
+    
+class CartIte(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
     
 
     
